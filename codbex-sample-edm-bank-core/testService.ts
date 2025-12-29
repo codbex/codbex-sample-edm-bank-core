e890a63311ce462965d9859a0e9dcfa6013bc55c
ThuF <jordan.p.pavlov@gmail.com>
2025-12-29 15:17:14 +0000

    Updates testService.ts

--- codbex-sample-edm-bank-core/testService.ts
+++ codbex-sample-edm-bank-core/testService.ts
@@ -8,7 +8,8 @@ const repository = new AccountRepository();
 const accountId = repository.create({
     customerId: 1,
     iban: UUID.random().substring(0, 34),
-    balance: parseInt('' + Math.random() * 1000)
+    balance: parseInt('' + Math.random() * 1000),
+    // currency: 'USD',
 });
 
 const accountEntity = repository.findById(accountId);
@@ -17,16 +18,30 @@ if (!accountEntity) {
     throw new Error(`Account entity with id = ${accountId} not found`);
 }
 
+accountEntity.balance += 1234500;
+
+repository.update(accountEntity);
+
 const allAccounts = repository.findAll();
 
 const filteredAccounts = repository.findAll({
-    // conditions: [
-    //     {
-    //         operator: Operator.GE,
-    //         propertyName: 'balance',
-    //         value: 10000
-    //     }
-    // ],
+    conditions: [
+        {
+            operator: Operator.LE,
+            propertyName: 'balance',
+            value: 400
+        },
+        {
+            operator: Operator.LIKE,
+            propertyName: 'iban',
+            value: '%38%'
+        },
+        {
+            operator: Operator.IN,
+            propertyName: 'currency',
+            value: ['EUR', 'USD']
+        }
+    ],
     sorts: [
         {
             direction: Direction.DESC,
@@ -35,17 +50,11 @@ const filteredAccounts = repository.findAll({
     ],
 })
 
-const JsonHelper = Java.type("org.eclipse.dirigible.components.base.helpers.JsonHelper");
-const DynamicQueryFilter = Java.type("org.eclipse.dirigible.components.data.store.DynamicQueryFilter");
-
-const result = JsonHelper.fromJson(JSON.stringify({}), DynamicQueryFilter.QueryOptions.class);
-
-Response.println(`Result = ${result}`);
-Response.println(`-------`);
 Response.println(`Account id = ${accountId}`);
 Response.println(`-------`);
 Response.println(`Account entity = ${JSON.stringify(accountEntity, null, 4)}`);
 Response.println(`-------`);
 Response.println(`All accounts count = ${allAccounts.length}`);
 Response.println(`-------`);
-Response.println(`Filtered Accounts (${filteredAccounts.length}) = ${JSON.stringify(filteredAccounts.map(e => { return { iban: e.iban, balance: e.balance } }), null, 4)}`);
+Response.println(`Filtered Accounts (${filteredAccounts.length}) = ${JSON.stringify(filteredAccounts.map(e => { return { iban: e.iban, balance: e.balance, currency: e.currency } }), null, 4)}`);
+Response.println(`-------`);
